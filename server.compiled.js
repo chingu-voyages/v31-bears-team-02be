8fb0a6ad7864b9db5993c78d832f27d8e864d10f
maxbildner <mbildner@icloud.com>
2021-06-10 18:32:03 -0400

    rebuild

--- server.compiled.js
+++ server.compiled.js
@@ -11,7 +11,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 var PORT = process.env.HTTP_PORT || 4001; // const PORT = process.env.PORT || 4001;
 
 var app = (0, _express["default"])();
-app.use(_express["default"]["static"](_path["default"].join(__dirname, 'client', 'build')));
+app.use(_express["default"]["static"](_path["default"].join(__dirname, 'frontend', 'build')));
 app.get('/', function (req, res) {
   res.send('just gonna send it');
 });
