46c9b56bcf573c43ae1aabd077fa2a7162f1a60e
maxbildner <mbildner@icloud.com>
2021-06-10 16:27:30 -0400

    build server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
-
-// express = backend web application framework 
+//
+import path from 'path';
 import express from 'express';
 
 const PORT = process.env.HTTP_PORT || 4001;
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 
 app.get('/', (req, res) => {
