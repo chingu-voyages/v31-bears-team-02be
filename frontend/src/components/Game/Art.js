dbc3f6fe21bedf1c9aaf543fc9570b21af8316c7
Ken Cruz <ken@kencruz.ca>
2021-06-19 14:19:32 -0400

    Added magnifying image transition to high quality on load

--- frontend/src/components/Game/Art.js
+++ frontend/src/components/Game/Art.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import Magnifier from "react-magnifier";
 
 function Art({ art, correctArt }) {
   console.log("correctArt: ", correctArt);
   console.log(art);
 
+  const [largeImgLoaded, setLargeImgLoaded] = useState(false);
+  const largeImg = new Image();
+  largeImg.src = correctArt.primaryImage;
+  largeImg.onload = () => setLargeImgLoaded(true);
+
   return (
     <div className="art-container">
       <Magnifier
         src={correctArt.primaryImageSmall}
-        zoomImgSrc={correctArt.primaryImage}
+        zoomImgSrc={
+          largeImgLoaded
+            ? correctArt.primaryImage
+            : correctArt.primaryImageSmall
+        }
         alt={"art file"}
       />
       <p>{correctArt.artistDisplayName}</p>
