cd0478702d9b03d7ec0956a48c64a378ca1b85b5
Ken Cruz <ken@kencruz.ca>
2021-06-18 22:16:43 -0400

    added magnifying glass component to image

--- frontend/src/components/Game/Art.js
+++ frontend/src/components/Game/Art.js
@@ -1,10 +1,12 @@
+import Magnifier from "react-magnifier";
+
 function Art({ art, correctArt }) {
   console.log("correctArt: ", correctArt);
   console.log(art);
 
   return (
     <div className="art-container">
-      <img src={correctArt.primaryImageSmall} alt="art file" />
+      <Magnifier src={correctArt.primaryImageSmall} alt={"art file"} />
       <p>{correctArt.artistDisplayName}</p>
     </div>
   );
