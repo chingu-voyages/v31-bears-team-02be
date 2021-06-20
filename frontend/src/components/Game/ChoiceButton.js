9b7873a7c7fe48e91a5c2435648b8a3aac64c43e
mcardona9015 <mcardona9015@gmail.com>
2021-06-19 22:52:54 -0400

    adjusted game component layout and changed navbar height to 10vh

--- frontend/src/components/Game/ChoiceButton.js
+++ frontend/src/components/Game/ChoiceButton.js
@@ -1,5 +1,12 @@
 const ChoiceButton = ({ artistName, handleClick }) => {
-  return <input type="button" onClick={handleClick} value={artistName}></input>;
+  return (
+    <input
+      className="choice-button"
+      type="button"
+      onClick={handleClick}
+      value={artistName}
+    ></input>
+  );
 };
 
 export default ChoiceButton;
