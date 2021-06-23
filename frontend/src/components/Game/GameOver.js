b5a740bccc1a680e50456345a075ccf89d7131b5
mcardona9015 <mcardona9015@gmail.com>
2021-06-23 15:44:04 -0400

    began work on game over image gallery. Implemented some scss rules and links to the images met page

--- frontend/src/components/Game/GameOver.js
+++ frontend/src/components/Game/GameOver.js
@@ -1,12 +1,26 @@
-const GameOver = ({ roundHistory }) => {
+const GameOver = ({ roundHistory, allCorrectArt }) => {
+  console.log("allCorrectArt: ", allCorrectArt);
   function reloadPage() {
     window.location.reload();
   }
+
+  const allImages = allCorrectArt.map((art) => {
+    return (
+      <a href={art.objectURL} target="_blank" rel="noreferrer">
+        <div key={art.objectID} className="image-container">
+          <div className="frame">
+            <img src={art.primaryImage} alt="correct art" />
+          </div>
+        </div>
+      </a>
+    );
+  });
 
   return (
     <div className="game-over">
       <h3>Game Over</h3>
       <h3>Total Correct: {roundHistory.filter((x) => x === "✔").length}</h3>
+      <div className="correct-art-container">{allImages}</div>
       <div className="button-container">
         <button onClick={reloadPage}>Play again</button>
       </div>
