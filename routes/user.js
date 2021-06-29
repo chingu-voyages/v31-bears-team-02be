3109ac36e5293eaff4532c566a00b9517aaeec53
mcardona9015 <mcardona9015@gmail.com>
2021-06-29 17:16:25 -0400

    changed import and export method of express in user route

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import UserController from '../controller/user';
+// import express from 'express';
+import UserController from "../controller/user";
+
+const express = require("express");
 
 // Router for /user endpoint
 const userRouter = express.Router();
 // Here '/' means the root of path 'user/'
-userRouter.route('/')
-  .post(express.json(), UserController.createUser);
+userRouter.route("/").post(express.json(), UserController.createUser);
 // This is route '/user/auth'
-userRouter.route('/auth')
-  .post(express.json(), UserController.authenticateUser);
+userRouter.route("/auth").post(express.json(), UserController.authenticateUser);
 
-export default userRouter;
+// export default userRouter;
+module.exports = userRouter;
