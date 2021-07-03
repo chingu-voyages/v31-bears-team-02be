818417a67ca6e439c4e70e558b583fc186b659ca
Ken Cruz <ken@kencruz.ca>
2021-07-03 18:40:40 -0400

    implemented game quit prompt before leaving game page

--- frontend/src/components/Game/Art.js
+++ frontend/src/components/Game/Art.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Magnifier from "react-magnifier";
+import { Prompt } from "react-router-dom";
 // import ReactImageMagnify from "react-image-magnify";
 
 function Art({ art, correctArt }) {
@@ -7,6 +8,8 @@ function Art({ art, correctArt }) {
   console.log(art);
 
   const [image, setImage] = useState(null);
+  const promptMsg =
+    "Are you sure you want to quit? Game progress is saved and will be loaded the next time you play.";
 
   useEffect(() => {
     const smallImg = new Image();
@@ -16,6 +19,20 @@ function Art({ art, correctArt }) {
     largeImg.src = correctArt.primaryImage;
     largeImg.onload = () => setImage(largeImg);
   }, [correctArt]);
+
+  useEffect(() => {
+    const promptHandler = (e) => {
+      e.preventDefault();
+      e.returnvalue = promptMsg;
+      return promptMsg;
+    };
+
+    window.addEventListener("beforeunload", promptHandler);
+
+    return () => {
+      window.removeEventListener("beforeunload", promptHandler);
+    };
+  }, []);
 
   return (
     <div className="art-container">
@@ -32,6 +49,7 @@ function Art({ art, correctArt }) {
       )}
       {/* </div> */}
       {/* <span>{correctArt.artistDisplayName}</span> */}
+      <Prompt message={() => promptMsg} />
     </div>
   );
 }
