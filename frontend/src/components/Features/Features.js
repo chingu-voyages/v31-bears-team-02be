8c5a7bc41e12146f7d71fce38796070a8632ea17
Ken Cruz <ken@kencruz.ca>
2021-08-11 22:48:21 -0400

    added padding to figcaptions for smaller screen widths

--- frontend/src/components/Features/Features.js
+++ frontend/src/components/Features/Features.js
@@ -14,7 +14,7 @@ const Features = () => (
             alt="placeholder"
             src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/435868/800382/main-image"
           />
-          <figcaption className="sm:max-w-sm sm:mx-6 lg:max-w-none lg:mx-0">
+          <figcaption className="px-4 sm:px-0 sm:max-w-sm sm:mx-6 lg:max-w-none lg:mx-0">
             Each artwork is a puzzle to be solved. Can you guess who created it?
             Simply hit play for a quick round or create an account and keep
             track of your score.
@@ -30,7 +30,7 @@ const Features = () => (
             alt="placeholder"
             src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/436529/795979/main-image"
           />
-          <figcaption className="sm:max-w-sm sm:mx-6 lg:max-w-none lg:mx-0">
+          <figcaption className="px-4 sm:px-0 sm:max-w-sm sm:mx-6 lg:max-w-none lg:mx-0">
             Study every detail, get to know new artists, and learn new things
             about the ones you know. Play again and show the knowledge
             you&apos;ve gathered and get higher scores. Share your discoveries
@@ -47,7 +47,7 @@ const Features = () => (
             alt="placeholder"
             src="https://collectionapi.metmuseum.org/api/collection/v1/iiif/10150/35243/main-image"
           />
-          <figcaption className="sm:max-w-sm sm:mx-6 lg:max-w-none lg:mx-0">
+          <figcaption className="px-4 sm:px-0 sm:max-w-sm sm:mx-6 lg:max-w-none lg:mx-0">
             Thousands of art pieces and artifacts are waiting for you to
             discover them. Play and let the game surprise you with artworks to
             identify, or take your own route by looking through information
